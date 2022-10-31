a6242e2b8537fcc14fa3ac11bd2bbb2b19aa6386
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-10-31 20:14:16 +0100

    refactor: change urls to automatically switch between deployments and production

--- client/src/utils/apiActions/index.js
+++ client/src/utils/apiActions/index.js
@@ -2,10 +2,9 @@
 // export const backend_url = "http://localhost:5000/api"
 // export const frontend_url = "http://localhost:3000/"
 
-
-// Online
-export const backend_url = "https://lmcp.herokuapp.com/api"
-export const frontend_url = "https://lmcp.vercel.app/"
+// 
+export const backend_url = process.env.NODE_ENV === "development" ? "http://localhost:5000/api": "https://surpluswap.herokuapp.com/"
+export const frontend_url = process.env.NODE_ENV === "development" ? "http://localhost:3000/": "https://surpluswap.vercel.app/"
 
 export const setError = (err) => {
     return err.response && err.response.data.message ? err.response.data.message : err.message
