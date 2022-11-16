657832160b48b99523a8a42bec53eb5e8ae992db
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-11-16 16:33:46 +0100

    refactor: changed trade with us route

--- client/src/components/Intro/index.js
+++ client/src/components/Intro/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import { useSelector } from 'react-redux'
 import { motion } from "framer-motion"
 
 import styles from "./intro.module.css"
@@ -11,13 +12,15 @@ import {
   } from "../../assets"
 
 const Intro = () => {
+  const { currentUser } =  useSelector(state => state.userStore)
+
   return (
     <div className={`${styles.intro}`}>
         <div className={`${styles.container} ${styles.intro_container}`}>
         <div className={styles.intro_col1}>
             <h1 className="spacing-sm">Impex Funds</h1>
             <p className="spacing-md">Enjoy seamless, unlimited and swift payments for all of your RMB, Gift Cards and Crypto transactions.</p>
-            <Link to="/user/signup">
+            <Link to={currentUser._id ? "/dashboard" :"/user/signup" }>
               <Button variant="primary">TRADE WITH US TODAY</Button>
             </Link>
         </div>
