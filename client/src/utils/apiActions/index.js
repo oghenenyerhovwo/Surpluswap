3545ecb0e67569023869bb841633e8f747a6b61c
oghenenyerhovwo <93879454+oghenenyerhovwo@users.noreply.github.com>
2022-11-24 20:56:38 +0100

    swicth: switching fully to production mode

--- client/src/utils/apiActions/index.js
+++ client/src/utils/apiActions/index.js
@@ -3,8 +3,8 @@
 // export const frontend_url = "http://localhost:3000/"
 
 // 
-export const backend_url = process.env.NODE_ENV === "development" ? "http://localhost:5000/api": "https://surpluswap.herokuapp.com/"
-export const frontend_url = process.env.NODE_ENV === "development" ? "http://localhost:3000/": "https://surpluswap.vercel.app/"
+export const backend_url = "https://surpluswap.herokuapp.com/"
+export const frontend_url =  "https://surpluswap.vercel.app/"
 
 export const setError = (err) => {
     return err.response && err.response.data.message ? err.response.data.message : err.message
